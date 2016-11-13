88cff4238356c393e35eeb10914e1f0143ae1a1a
Tom <tuomis29@gmail.com>
2016-11-13 00:12:01 +0000

    completed API

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var app = express();
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.set("view engine","ejs");
 
 app.get("/",function(req,res){
-   res.send("HELLO WORLD!"); 
+   res.render("index.ejs");
 });
+
+app.post("/getFileSize", upload.single('file'),function(req,res){
+    var json = {
+        size:req.file.size
+    };
+    res.send(json);
+});
+
 
 app.listen(process.env.PORT || 8080, function(err){
     if(err) return console.log(err);
