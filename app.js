c547ebeecd74661541bcaa63a638c0241e992d79
Tom <tuomis29@gmail.com>
2016-11-13 00:16:03 +0000

    replaced file uploading with sotring in memory as Buffer obj

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var app = express();
 var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 var bodyParser = require('body-parser');
 
 
